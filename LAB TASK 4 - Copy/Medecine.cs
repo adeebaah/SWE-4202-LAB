eec855fb31f1b3d65386bb1ceda5ed0cc12b1c98
Adeba Islam <adebaislam@iut-dhaka.edu>
2022-01-05 21:22:24 +0600

    Pharmacy Management System

---
+++ LAB TASK 4 - Copy/Medecine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_TASK_4
+{
+    public class Medecine
+    {
+        public int id;
+        public string name;
+        public string power;
+        public int quantity;
+        public int price;
+        public int balance=0;
+
+    }
+}
---
+++ LAB TASK 4/Balance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_TASK_4
+{
+    internal class Balance
+    {
+        public int totalbalance = 0;
+    }
+}
